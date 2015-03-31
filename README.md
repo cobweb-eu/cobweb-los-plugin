134e11fc3611c5a854906d22a29715870747ff8e
conormuldoon <conor.muldoon@gmail.com>
2015-03-31 15:49:17 +0100

    Initial commit for COBWEB LOS Plug-in

---
+++ README.md
@@ -0,0 +1,4 @@
+cobweb-los-plugin
+=====================
+
+COBWEB plug-in for line-of-sight in Android
---
+++ src/android/COBWEBSensorPlugin.java
@@ -0,0 +1,101 @@
+package ie.ucd.cobweb.cordova;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
+import android.os.Build;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.content.Context;
+import android.hardware.*;
+
+import java.util.*;
+
+public class COBWEBSensorPlugin extends CordovaPlugin implements
+		SensorEventListener {
+
+	private float[] accel, orient;
+	final private static int NMAT = 9;
+
+	// Actions
+	final private static String LOS = "lineOfSight";
+	final private static String DEVI = "deviceInfo";
+
+	// Device meta data
+	final private static String MKMOD = "Make and Model";
+	final private static String OSVER = "OS Version";
+	final private static char SP = ' ';
+
+	// Compass meta data
+	private static final String AZIMUTH = "Azimuth";
+	private static final String PITCH = "Pitch";
+	private static final String ROLL = "Roll";
+
+	@Override
+	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+		super.initialize(cordova, webView);
+		SensorManager mSensorManager = (SensorManager) this.cordova
+				.getActivity().getSystemService(Context.SENSOR_SERVICE);
+		Sensor mCompass = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+		mSensorManager.registerListener(this, mCompass,
+				SensorManager.SENSOR_STATUS_ACCURACY_HIGH);
+		Sensor mAccelerometer = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mSensorManager.registerListener(this, mAccelerometer,
+				SensorManager.SENSOR_STATUS_ACCURACY_HIGH);
+	}
+
+	@Override
+	public boolean execute(String action, JSONArray args,
+			CallbackContext callbackContext) throws JSONException {
+		if (action.equals(LOS)) {
+			if (orient == null)
+				return false;
+			JSONObject compass = new JSONObject();
+			compass.put(AZIMUTH, orient[0]);
+			compass.put(PITCH, orient[1]);
+			compass.put(ROLL, orient[2]);
+			callbackContext.success(compass);
+			return true;
+
+		} else if (action.equals(DEVI)) {
+			JSONObject info = new JSONObject();
+			String mkM = Build.MODEL.toUpperCase();
+			if (!mkM.startsWith(Build.MANUFACTURER.toUpperCase()))
+				mkM = Build.MANUFACTURER.toUpperCase() + SP + mkM;
+			info.put(MKMOD, mkM);
+			info.put(OSVER, Build.VERSION.RELEASE);
+			callbackContext.success(info);
+			return true;
+		}
+		return false;
+
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+
+		int type = event.sensor.getType();
+		if (type == Sensor.TYPE_ACCELEROMETER) {
+			accel = event.values;
+		} else {
+			float[] magF = event.values;
+			if (accel != null) {
+				float[] rMat = new float[NMAT];
+				if (SensorManager.getRotationMatrix(rMat, null, accel, magF)) {
+					orient = new float[3];
+					SensorManager.getOrientation(rMat, orient);
+				}
+			}
+		}
+	}
+
+}
